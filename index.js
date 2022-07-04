6c4b37fc0e2aefa39a91de73937b13a00e4cc7e7
qkrsh <qkrtkdgus@gachon.ac.kr>
2022-07-04 20:53:05 +0900

    first commit

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ mongoose.connect('mongodb+srv://qkrsh:Nash256328!@cluster0.wntg5rm.mongodb.net/?
 }).then(()=>console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
 
-app.get('/', (req,res)=>res.send('Hello World! GO GO'))
+app.get('/', (req,res)=>res.send('Hello World! GO2 GO'))
 
 app.listen(port, ()=>console.log('Example app listeneing on port ${port}!'))
