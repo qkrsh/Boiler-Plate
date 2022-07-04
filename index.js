98281e88edc4f395a726d789eb8c543e5ad6a2d8
qkrsh <qkrtkdgus@gachon.ac.kr>
2022-07-04 20:24:54 +0900

    처음 저장소

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app=express()
+const port = 5000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://qkrsh:Nash256328!@cluster0.wntg5rm.mongodb.net/?retryWrites=true&w=majority', {
+    useNewUrlParser:true, useUnifiedTopology:true, useCreateIndex: true, useFindAndModify:false
+}).then(()=>console.log('MongoDB Connected...'))
+.catch(err => console.log(err))
+
+app.get('/', (req,res)=>res.send('Hello World! GO GO'))
+
+app.listen(port, ()=>console.log('Example app listeneing on port ${port}!'))
